54e908628c63cea2c8fcc73306743cc16d1c821a
MAC <user@MACs-MacBook-Pro.local>
2019-01-16 12:03:57 +0100

    CorsFilter refactored to CorsConfig to avert name conflict

--- src/main/java/com/activedge/usermgt/config/CorsConfig.java
+++ src/main/java/com/activedge/usermgt/config/CorsConfig.java
@@ -1,11 +1,14 @@
 package com.activedge.usermgt.config;
 
+import org.springframework.stereotype.Component;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class CorsFilter implements Filter {
+@Component
+public class CorsConfig implements Filter {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
