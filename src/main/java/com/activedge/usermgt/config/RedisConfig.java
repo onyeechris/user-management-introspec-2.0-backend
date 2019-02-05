2817de9304fb1e612984c58301872decf326d457
MAC <user@MACs-MacBook-Pro.local>
2019-02-05 13:04:44 +0100

    changed redis config to fix windows connectivity issue

--- src/main/java/com/activedge/usermgt/config/RedisConfig.java
+++ src/main/java/com/activedge/usermgt/config/RedisConfig.java
@@ -2,9 +2,17 @@ package com.activedge.usermgt.config;
 
 
 import com.github.caryyu.spring.embedded.redisserver.RedisServerConfiguration;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.FatalBeanException;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import redis.clients.jedis.JedisPoolConfig;
 import redis.embedded.RedisServer;
 
 import javax.annotation.PostConstruct;
@@ -16,13 +24,55 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 
 @Configuration
 //@EnableRedisRepositories(basePackages = "com.activedge.usermgt.repository.redis")
-public class RedisConfig {
+public class RedisConfig implements DisposableBean, EnvironmentAware, InitializingBean {
 
-    @Bean
-    public RedisServerConfiguration redisServerConfiguration() {
-        return new RedisServerConfiguration();
+    private Log log = LogFactory.getLog(this.getClass());
+    private RedisServer redisServer;
+    private Environment environment;
+
+    public int getPort() {
+        int v = environment.getProperty("spring.redis.port",Integer.class,0);
+        v = v == 0 ? environment.getProperty("global.redis.port",Integer.class,6379) : v;
+        return v;
     }
 
+    private boolean isEmbedded() {
+        Boolean v = environment.getProperty("spring.redis.embedded",Boolean.class,null);
+        v = v == null ? environment.getProperty("global.redis.embedded",Boolean.class,false) : v;
+        return v;
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        if(redisServer != null) {
+            redisServer.stop();
+            redisServer = null;
+        }
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        if (!isEmbedded()) {
+            return;
+        }
+
+        try {
+            int port = getPort();
+            redisServer = new RedisServer(port);
+            redisServer.start();
+
+            if(log.isInfoEnabled()) {
+                log.info("Starting local embedded redis server successfully, port is " + port);
+            }
+        } catch (IOException e) {
+            throw new FatalBeanException("Failed to start local embedded redis server ", e);
+        }
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
     /*
     private RedisServer redisServer;
 
