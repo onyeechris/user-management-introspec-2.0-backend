dd461841fec6154c8a8283d9a327967c8b196d43
Elvis O <elviso@activedgetechnologies.com>
2019-07-25 09:29:22 +0100

    added a hack for database idempotency, using database=usermgt

--- src/main/java/com/activedge/usermgt/UsermgtApplication.java
+++ src/main/java/com/activedge/usermgt/UsermgtApplication.java
@@ -1,24 +1,41 @@
 package com.activedge.usermgt;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.Properties;
 
 @EnableJpaAuditing
 @SpringBootApplication
 //public class UsermgtApplication extends WebMvcConfigurerAdapter {
 public class UsermgtApplication implements WebMvcConfigurer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UsermgtApplication.class, args);
+	public static void main(String[] args) throws SQLException {
+
+        SpringApplication application = new SpringApplication(UsermgtApplication.class);
+        // little hack to initialize schemas on first application startup
+        DataSource dataSource  = DataSourceBuilder
+                .create()
+                .username("postgres")
+                .password("passadmin")
+                .url("jdbc:postgresql://localhost:5432/usermgt")
+                .driverClassName("org.postgresql.Driver")
+                .build();
+
+        Properties properties = new Properties();
+
+        String initialize = dataSource.getConnection().getMetaData().getTables(null, null, "staff", null).next() ? "never" : "always";
+
+        properties.put("spring.datasource.initialization-mode", initialize);
+
+        application.setDefaultProperties(properties);
+        application.run(args);
+
 	}
 
 //	@Override
