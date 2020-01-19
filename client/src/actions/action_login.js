372e67c729dd21fe406f376d6296f4a502d7ff95
lawrence <lawrenceo@activedgetechnologies.com>
2020-01-19 21:18:37 +0100

    hot login fix

--- client/src/actions/action_login.js
+++ client/src/actions/action_login.js
@@ -1,40 +1,39 @@
-import {
-  BASE_URL
-} from './types';
+import { BASE_URL } from "./types";
 
-export const USER_FETCHED = 'USER_FETCHED';
-export const USER_FETCH_ERROR = 'USER_FETCH_ERROR';
+export const USER_FETCHED = "USER_FETCHED";
+export const USER_FETCH_ERROR = "USER_FETCH_ERROR";
 
-export function fetchUser(type, appUser) {
-  // console.log(appUser);
-  return (dispatch) => {
-    return new Promise((resolve, reject) => {
-
-      fetch(BASE_URL + type, {
-        method: 'POST',
-        body: JSON.stringify(appUser)
-      })
-        .then((response) => response.json())
-        .then((responseJSON) => {
-          resolve(responseJSON);
-          dispatch(loadUser(responseJSON));
-        })
-        .catch((error) => {
-          reject(error);
-          dispatch({
-            type: USER_FETCH_ERROR,
-            payload: "Login failure. Incorrect username or password"
-          });
-          console.log('Rejected.. Login Error');
-        })
-    })
-  }
+export function fetchUser (type, appUser) {
+	// console.log(appUser);
+	return (dispatch) => {
+		return new Promise((resolve, reject) => {
+			fetch(BASE_URL + type, {
+				method  : "POST",
+				headers : {
+					"Content-Type" : "application/json",
+					// 'Content-Type': 'application/x-www-form-urlencoded',
+				},
+				body    : JSON.stringify(appUser),
+			})
+				.then((response) => response.json())
+				.then((responseJSON) => {
+					resolve(responseJSON);
+					dispatch(loadUser(responseJSON));
+				})
+				.catch((error) => {
+					reject(error);
+					dispatch({
+						type    : USER_FETCH_ERROR,
+						payload : "Login failure. Incorrect username or password",
+					});
+				});
+		});
+	};
 }
 
-
-export function loadUser(results) {
-  return {
-    type: USER_FETCHED,
-    payload: results
-  }
+export function loadUser (results) {
+	return {
+		type    : USER_FETCHED,
+		payload : results,
+	};
 }
