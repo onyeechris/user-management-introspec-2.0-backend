71da2e19f0130bc14ff4ab9a47db4da9a861bc3e
lawrence <lawrenceo@activedgetechnologies.com>
2020-02-11 13:25:21 +0100

    cleared unsed codes

--- client/src/views/Pages/Login/Login.js
+++ client/src/views/Pages/Login/Login.js
@@ -2,28 +2,9 @@ import React, { Component } from "react";
 // import { Link } from "react-router-dom";
 import "../../../custom.css";
 import jwtDecode from "jwt-decode";
-import {
-	// Button,
-	Card,
-	CardBody,
-	CardGroup,
-	Col,
-	Container,
-	Input,
-	FormGroup,
-	Label,
-	Form,
-	// InputGroup,
-	// InputGroupAddon,
-	// InputGroupText,
-	Row,
-} from "reactstrap";
-//import { PostData } from "../../services/PostData";
+import { Card, CardBody, CardGroup, Col, Container, Input, FormGroup, Label, Form, Row } from "reactstrap";
 import { Redirect } from "react-router-dom";
-//import { fetchUser } from "../../../actions/action_login";
 import { FormattedMessage } from "react-intl";
-// import { SimpleReactValidator } from "simple-react-validator";
-// import { Values } from "redux-form-website-template";
 import LoginForm from "./LoginForm";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
@@ -43,14 +24,13 @@ class Login extends Component {
 			value              : "en",
 			staffData          : {},
 		};
-		// this.login = this.login.bind(this);
 		this.loginUser = this.loginUser.bind(this);
 		this.onChange = this.onChange.bind(this);
 	}
 
 	storedLanguage = "";
 
-	componentWillMount () {
+	componentDidMount () {
 		this.storedLanguage = localStorage.se8lementLang;
 		this.setState({ value: localStorage.se8lementLang });
 	}
@@ -62,25 +42,6 @@ class Login extends Component {
 		this.props.setLocale(event.target.value);
 	}
 
-	// login(event) {
-	//   event.preventDefault();
-	//   if (this.state.userLogin.username && this.state.userLogin.password) {
-	//     this.props.actions.fetchUser("auth", this.state.userLogin).then(result => {
-	//       sessionStorage.setItem("userData", JSON.stringify(result));
-	//       sessionStorage.setItem("loggedInUser", this.state.userLogin.username);
-	//       this.setState({ redirectToReferrer: true });
-	//       //decode the token
-	//       let userData = jwtDecode(result.token);
-	//       sessionStorage.setItem("userRole", userData.authorities[0]);
-	//     }, error => {
-	//       this.setState({ loginError: "Username or password incorrect!" })
-	//     }
-	//     )
-	//   } else {
-	//     this.setState({ loginError: "Please fill both fields!" })
-	//   }
-	// }
-
 	onChange (e) {
 		this.setState({ [e.target.name]: e.target.value });
 	}
@@ -89,15 +50,11 @@ class Login extends Component {
 		var userInfo = JSON.parse(JSON.stringify(this.state.userLogin));
 		userInfo[field] = event.target.value;
 		this.setState({ userLogin: userInfo });
-		// this.setState({ userLogin: userInfo }, e => //console.log(this.state.userLogin));
-		// //console.log(this.state.userLogin);
 	}
 
 	loginUser = (loginUserData) => {
-		//console.log(loginUserData);
 		this.props.actions.fetchUser("auth", loginUserData).then(
 			(result) => {
-				//console.log(result);
 				if (JSON.stringify(result).includes("401")) {
 					this.setState({ loginError: "Username or password incorrect!" });
 				} else if (JSON.stringify(result).includes("400") || JSON.stringify(result).includes("500")) {
@@ -109,17 +66,13 @@ class Login extends Component {
 
 					//decode the token
 					let userData = jwtDecode(result.token);
-					//console.log(userData);
 					sessionStorage.setItem("userRole", userData.authorities[0]);
 
 					// Get the user's data to store in session storage
 					this.props.fetchAllStaffs("?size=1000").then(
 						(result) => {
-							//console.log(result.data);
 							result.data.payload.forEach((staff) => {
-								//console.log(staff.first_name);
 								if (staff.email === loginUserData.username) {
-									//console.log(staff);
 									sessionStorage.setItem("loggedInUserData", JSON.stringify(staff));
 								}
 							});
@@ -161,8 +114,6 @@ class Login extends Component {
 		const errorStyle = {
 			color : "red",
 		};
-
-		//console.log(this.props);
 
 		if (this.state.redirectToReferrer) {
 			return <Redirect to={"/dashboard"} />;
@@ -210,54 +161,6 @@ class Login extends Component {
 												this.state.loginError
 											)}
 										</p>
-										{/* <form action="" method="post">
-                      <InputGroup className="mb-3">
-                        <InputGroupAddon addonType="prepend">
-                          <InputGroupText>
-                            <i className="icon-user" />
-                          </InputGroupText>
-                        </InputGroupAddon>
-                        <Input
-                          type="text"
-                          name="username"
-                          placeholder="Username"
-                          autoComplete="username"
-                          required
-                          onChange={this.updateValue.bind(this, 'username')}
-                        
-                        />
-                        
-                      </InputGroup>
-                      <InputGroup className="mb-4">
-                        <InputGroupAddon addonType="prepend">
-                          <InputGroupText>
-                            <i className="icon-lock" />
-                          </InputGroupText>
-                        </InputGroupAddon>
-                        <Input
-                          type="password"
-                          name="password"
-                          placeholder="Password"
-                          autoComplete="current-password"
-                          required
-                          onChange={this.updateValue.bind(this, 'password')}
-                        />
-                      </InputGroup>
-                      <p className="text-muted" class={errorStyle} style={{ color: 'red' }}>{this.state.loginError}</p>
-                      <Row>
-                        <Col xs="6">
-                          <Button
-                            type="submit"
-                            color="primary"
-
-                            className="px-4"
-                            onClick={this.login}
-                          >
-                            <FormattedMessage id="Login" defaultMessage="Login" />
-                          </Button>
-                        </Col>
-                      </Row>
-                    </form> */}
 
 										<LoginForm
 											onSubmit={this.loginUser}
