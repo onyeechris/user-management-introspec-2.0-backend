2dfa00875987fb21e7ae3d77b10b85d4dffcdc20
lawrence <lawrenceo@activedgetechnologies.com>
2020-03-10 16:16:15 +0100

    deleted buggy client package

--- client/src/actions/action_locale.js
+++
@@ -1,9 +0,0 @@
-export const LOCALE_SET = 'locale_set';
-
-export const setLocale = (language) => dispatch => {
-    localStorage.se8lementLang = language;
-    dispatch({
-        type: LOCALE_SET,
-        payload: language
-    })
-}
