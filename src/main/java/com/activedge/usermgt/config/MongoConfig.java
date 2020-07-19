1e49e68cfadd1960e00413297d99c3e2e8bf7b8c
Elvis O <elvisouk@yahoo.com>
2020-07-19 12:36:01 +0100

    added default mongodb uri

--- src/main/java/com/activedge/usermgt/config/MongoConfig.java
+++ src/main/java/com/activedge/usermgt/config/MongoConfig.java
@@ -2,7 +2,6 @@ package com.activedge.usermgt.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
