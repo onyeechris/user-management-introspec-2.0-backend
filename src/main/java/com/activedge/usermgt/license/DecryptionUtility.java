70f4a1b39514fae35383c56de0cf468086820b91
adeybabs <adeybabs.aa@gmail.com>
2023-12-07 10:11:29 +0100

    updated decrypt code

---
+++ src/main/java/com/activedge/usermgt/license/DecryptionUtility.java
@@ -0,0 +1,52 @@
+package com.activedge.usermgt.license;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.file.StandardOpenOption;
+import java.security.Key;
+import java.util.Base64;
+import javax.crypto.spec.IvParameterSpec;
+
+
+public class DecryptionUtility {
+
+
+        public static byte[] decryptLicense(byte[] encryptedData, String key) {
+            try {
+                // The decryption logic goes here
+                // Use the provided encryptedData and key to decrypt the license file
+
+                // Example implementation:
+                // return SomeDecryptionUtility.decrypt(encryptedData, key);
+
+                // Ensure to handle exceptions and return null in case of decryption failure
+
+                // Decode Base64
+                byte[] decodedData = Base64.getDecoder().decode(encryptedData);
+
+                // Extract IV (Initialization Vector) from the first 16 bytes
+                byte[] ivBytes = new byte[16];
+                System.arraycopy(decodedData, 0, ivBytes, 0, 16);
+                IvParameterSpec iv = new IvParameterSpec(ivBytes);
+
+                // Extract encrypted content (excluding IV)
+                byte[] encryptedContent = new byte[decodedData.length - 16];
+                System.arraycopy(decodedData, 16, encryptedContent, 0, encryptedContent.length);
+
+                // Create SecretKeySpec from the key
+                SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(), "AES");
+
+                // Initialize Cipher for decryption
+                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+                cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, iv);
+
+                // Decrypt the content
+                return cipher.doFinal(encryptedContent);
+            } catch (Exception e) {
+                e.printStackTrace();
+                // Handle decryption failure
+                return null;
+            }
+        }
+    }
--- src/main/java/com/activedge/usermgt/license/LicenseRouteBuilder.java
+++ src/main/java/com/activedge/usermgt/license/LicenseRouteBuilder.java
@@ -4,6 +4,9 @@ import org.apache.camel.Exchange;
 import org.springframework.stereotype.Component;
 import org.apache.camel.builder.RouteBuilder;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 @Component
 public class LicenseRouteBuilder extends RouteBuilder{
 
@@ -14,7 +17,7 @@ public class LicenseRouteBuilder extends RouteBuilder{
                 .when(header("CamelFileNameOnly").startsWith("Bank GPG"))
                 .process(this::decryptLicenseFile)
                 .when(header("CamelFileNameOnly").startsWith("KEY-Bank"))
-                .process(this::decryptKeyFile)
+                .process(this::readEncryptionKey)
                 .otherwise()
                 .log("Unknown file type: ${header.CamelFileNameOnly}")
                 .end();
@@ -23,11 +26,11 @@ public class LicenseRouteBuilder extends RouteBuilder{
     private void decryptLicenseFile(Exchange exchange) throws Exception {
         // Extract necessary data from the exchange
         byte[] encryptedData = exchange.getIn().getBody(byte[].class);
-        // Assuming the key is hardcoded for simplicity; replace with secure key management
-        byte[] keyBytes = "YourLicenseDecryptionKey".getBytes();
 
-        // Perform license decryption logic...
-        byte[] decryptedData = yourDecryptionMethod(encryptedData, keyBytes);
+        // Retrieve the encryption key from the exchange properties
+        String encryptionKey = exchange.getProperty("encryptionKey", String.class);
+
+        byte[] decryptedData = DecryptionUtility.decryptLicense(encryptedData, encryptionKey);
 
         // Example: Print a message after decryption
         System.out.println("License file decrypted successfully.");
@@ -36,30 +39,18 @@ public class LicenseRouteBuilder extends RouteBuilder{
         exchange.getIn().setBody(decryptedData);
     }
 
-    private void decryptKeyFile(Exchange exchange) throws Exception {
-        // Extract necessary data from the exchange
-        byte[] encryptedData = exchange.getIn().getBody(byte[].class);
-        // Assuming the key is hardcoded for simplicity; replace with secure key management
-        byte[] keyBytes = "YourKeyDecryptionKey".getBytes();
+    private void readEncryptionKey(Exchange exchange) throws Exception {
+        // Read the contents of the KEY-Bank file
+        String keyFilePath = exchange.getIn().getHeader(Exchange.FILE_PATH, String.class);
 
-        // Perform key decryption logic...
-        byte[] decryptedData = yourDecryptionMethod(encryptedData, keyBytes);
+        String encryptionKey = new String(Files.readAllBytes(Paths.get(keyFilePath)));
 
-        // Example: Print a message after decryption
-        System.out.println("Key file decrypted successfully.");
+        // Store the encryption key in exchange properties for later use
+        exchange.setProperty("encryptionKey", encryptionKey);
 
-        // Set the decrypted data as the new body of the exchange
-        exchange.getIn().setBody(decryptedData);
-    }
-
-    // Replace this with your actual decryption method
-    private byte[] yourDecryptionMethod(byte[] encryptedData, byte[] keyBytes) {
-        // Implement your decryption logic using the provided key
-        // For example, you can use a cryptographic library like Bouncy Castle or Java's javax.crypto
-        // This is a simplified example, and you should replace it with your actual decryption code
-        // ...
-
-        return decryptedData;
+        // Example: Print a message after reading the key
+        System.out.println("Encryption key read successfully.");
     }
 }
+
 
