a6f64ff91e7bd0a250df8cf998c7f26dcb0fe515
adeybabs <adeybabs.aa@gmail.com>
2023-12-07 13:19:55 +0100

    dependency check

--- src/main/java/com/activedge/usermgt/util/LoggingInterceptor.java
+++ src/main/java/com/activedge/usermgt/util/LoggingInterceptor.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 @Slf4j
 @Component
 public class LoggingInterceptor implements HandlerInterceptor {
