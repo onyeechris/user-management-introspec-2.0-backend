c7a7088ba22048c8e1dc337a829d8328003f6e43
adeybabs <adeybabs.aa@gmail.com>
2024-02-29 22:31:58 +0100

    routebuilder reconfigured

---
+++ src/main/java/com/activedge/usermgt/license/LicenseRouteBuilder.java
@@ -0,0 +1,11 @@
+package com.activedge.usermgt.license;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class LicenseRouteBuilder extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+    }
+}
