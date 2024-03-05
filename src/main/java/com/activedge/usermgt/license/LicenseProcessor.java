7752a7968bbeb2561959ba0362e6ded0ce617547
adeybabs <adeybabs.aa@gmail.com>
2024-03-06 00:45:33 +0100

    license processor

--- src/main/java/com/activedge/usermgt/license/LicenseProcessor.java
+++ src/main/java/com/activedge/usermgt/license/LicenseProcessor.java
@@ -1,10 +1,10 @@
 package com.activedge.usermgt.license;
 
 public class LicenseProcessor {
-    public static void processLicense(License myData) {
-        String licenseId = myData.getId();
-        String licenseKey = myData.getLicence();
-        String expirationDate = myData.getExpiry();
+    public static void processLicense(License license) {
+        String licenseId = license.getId();
+        String licenseKey = license.getLicence();
+        String expirationDate = license.getExpiry();
 
         // Add your logic to process the license information
         // Example: licenseService.processLicense(licenseId, licenseKey, expirationDate);
