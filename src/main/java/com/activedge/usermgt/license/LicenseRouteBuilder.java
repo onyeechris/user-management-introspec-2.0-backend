b8d8defd0dceb4ac21e70badaa239764160ee7a5
adeybabs <adeybabs.aa@gmail.com>
2024-03-08 16:24:08 +0100

    routebuilder working perfectly

--- src/main/java/com/activedge/usermgt/license/LicenseRouteBuilder.java
+++ src/main/java/com/activedge/usermgt/license/LicenseRouteBuilder.java
@@ -17,42 +17,42 @@ public class LicenseRouteBuilder extends RouteBuilder {
     @Value("${file.location}")
     private String fileLocation;
 
+//    @Override
+//    public void configure() throws Exception {
+//        from("file:" + fileLocation)
+////        from("file:/home/adedamolababatunde/Documents/personal-files/license/file/downloads")
+//                .log("Processing file: ${headers.CamelFileName}")
+//                // Your processing logic goes here
+//                .log("File processed successfully!")
+//                .end();
+//    }
+//}
+
     @Override
     public void configure() throws Exception {
         from("file:" + fileLocation)
-//        from("file:/home/adedamolababatunde/Documents/personal-files/license/file/downloads")
-                .log("Processing file: ${headers.CamelFileName}")
-                // Your processing logic goes here
-                .log("File processed successfully!")
-                .end();
+                .routeId("fileRoute")
+                .log("File received: ${headers.CamelFileName}")
+                .choice()
+                .when(header("CamelFileName").startsWith("Eco-Bank"))
+                        .log("Processing file: ${headers.CamelFileName}")
+                        .log("File content before unmarshal: ${body}")
+                        .unmarshal().json(JsonLibrary.Gson, Map.class) // Use Gson for JSON parsing
+                        .log("File content after unmarshal: ${body}")
+                        .process(exchange -> {
+                            // Access the parsed JSON data
+                            Message message = exchange.getMessage();  // Use getMessage() instead of getIn()
+                            Map<String, Object> jsonData = message.getBody(Map.class);
+                            // Further processing...
+
+                            // Additional logging to output the body of the message
+                            logger.info("File content: {}", jsonData);
+                        })
+                        .otherwise()
+                        .log("Skipping file: ${headers.CamelFileName}")
+                        .end();
     }
 }
-
-//    @Override
-//    public void configure() throws Exception {
-//        from("file:{{file.location}}")
-//                .routeId("fileRoute")
-//                .log("File received: ${headers.CamelFileName}")
-//                .choice()
-//                .when(header("CamelFileName").startsWith("Eco Bank"))
-//                        .log("Processing file: ${headers.CamelFileName}")
-//                        .log("File content before unmarshal: ${body}")
-//                        .unmarshal().json(JsonLibrary.Gson, Map.class) // Use Gson for JSON parsing
-//                        .log("File content after unmarshal: ${body}")
-//                        .process(exchange -> {
-//                            // Access the parsed JSON data
-//                            Message message = exchange.getMessage();  // Use getMessage() instead of getIn()
-//                            Map<String, Object> jsonData = message.getBody(Map.class);
-//                            // Further processing...
-//
-//                            // Additional logging to output the body of the message
-//                            logger.info("File content: {}", jsonData);
-//                        })
-//                        .otherwise()
-//                        .log("Skipping file: ${headers.CamelFileName}")
-//                        .end();
-//    }
-//}
 
 
 
